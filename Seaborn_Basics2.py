18211dd12fadc7c1885488d1d9aca04d00586470
Tanish Gupta <112066172+gtanish2003@users.noreply.github.com>
2023-11-16 15:21:10 +0530

    Delete Seaborn_Basics2.py

--- Seaborn_Basics2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import seaborn as sns
-from matplotlib import  pyplot  as plt
-
-file=pd.read_csv('excel1.csv')
-print(file.head())
-
-sns.barplot(x='Name',y='Matches',data=file,hue='Run',palette='rocket')
-plt.show()
